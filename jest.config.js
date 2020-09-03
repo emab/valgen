45bb5bc6d58dd4120119036bc98a8b4a06fd3a85
Eddy Brown <ebrown@scottlogic.com>
2020-09-03 19:29:17 +0100

    Add types and tailwind

--- jest.config.js
+++ jest.config.js
@@ -58,7 +58,7 @@ module.exports = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  // },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -79,7 +79,7 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  // },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -135,7 +135,7 @@ module.exports = {
   // testEnvironment: "jest-environment-jsdom",
 
   // Options that will be passed to the testEnvironment
-  // testEnvironmentOptions: {},
+  // },
 
   // Adds a location field to test results
   // testLocationInResults: false,
---
+++ src/valuesArray.ts
@@ -0,0 +1,123 @@
+import { Value } from './types/Value';
+
+export const VALUES: Value[] = [
+  { name: 'Accountability', level: 4 },
+
+  { name: 'Ambitious', level: 3 },
+
+  { name: 'Balance (home/work)', level: 4 },
+
+  { name: 'Balance (physical/emotional)', level: 6 },
+
+  { name: 'Being liked (L)', level: 2, limiting: true },
+
+  { name: 'Belonging', level: 2 },
+
+  { name: 'Caution (L)', level: 1, limiting: true },
+
+  { name: 'Community service', level: 6 },
+
+  { name: 'Competitive', level: 3 },
+
+  { name: 'Control (L)', level: 1, limiting: true },
+
+  { name: 'Cooperation', level: 5 },
+
+  { name: 'Counselling', level: 6 },
+
+  { name: 'Courage', level: 4 },
+
+  { name: 'Creativity', level: 5 },
+
+  { name: 'Dialogue', level: 6 },
+
+  { name: 'Ease with uncertainty', level: 7 },
+
+  { name: 'Education', level: 3 },
+
+  { name: 'Efficiency', level: 3 },
+
+  { name: 'Empathy', level: 6 },
+
+  { name: 'Enthusiasm', level: 5 },
+
+  { name: 'Environmental stewardship', level: 6 },
+
+  { name: 'Ethics', level: 7 },
+
+  { name: 'Excellence', level: 5 },
+
+  { name: 'Extreme Courtesy', level: 2 },
+
+  { name: 'Family / Friendship', level: 2 },
+
+  { name: 'Focus', level: 3 },
+
+  { name: 'Forgiveness', level: 7 },
+
+  { name: 'Future generations', level: 7 },
+
+  { name: 'Global perspective', level: 7 },
+
+  { name: 'Harmony', level: 2 },
+
+  { name: 'Honesty', level: 5 },
+
+  { name: 'Humour', level: 5 },
+
+  { name: 'Image (L)', level: 3, limiting: true },
+
+  { name: 'Independence', level: 4 },
+
+  { name: 'Integrity', level: 5 },
+
+  { name: 'Interdependence', level: 6 },
+
+  { name: 'Knowledge', level: 4 },
+
+  { name: 'Learning', level: 4 },
+
+  { name: 'Long-term persistence', level: 7 },
+
+  { name: 'Making a difference', level: 6 },
+
+  { name: 'Meaning', level: 5 },
+
+  { name: 'Nutrition', level: 1 },
+
+  { name: 'Open communication', level: 2 },
+
+  { name: 'Perseverance', level: 4 },
+
+  { name: 'Personal development', level: 4 },
+
+  { name: 'Physical exercise', level: 1 },
+
+  { name: 'Pride', level: 3 },
+
+  { name: 'Productivity', level: 3 },
+
+  { name: 'Respect', level: 2 },
+
+  { name: 'Responsibility', level: 4 },
+
+  { name: 'Security', level: 1 },
+
+  { name: 'Self-discipline', level: 1 },
+
+  { name: 'Self-improvement', level: 3 },
+
+  { name: 'Shared vision', level: 5 },
+
+  { name: 'Social responsibility', level: 6 },
+
+  { name: 'Status (L)', level: 3, limiting: true },
+
+  { name: 'Tradition (L)', level: 2, limiting: true },
+
+  { name: 'Trust', level: 5 },
+
+  { name: 'Wealth', level: 1 },
+
+  { name: 'Wisdom', level: 7 },
+];
