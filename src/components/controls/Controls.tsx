84891f88ba75c668e38dc7f8f5a0ad4053134d1d
Eddy Brown <eddybrown@me.com>
2020-09-06 18:09:30 +0100

    Show a tick when tab is complete

--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -1,18 +1,23 @@
 import { Button } from '@material-ui/core';
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, connect } from 'react-redux';
 import { Tab } from '../../types/Tab';
 import { resetValues } from '../tabs/store/actions';
 import ControlButton from './ControlButton';
 import domtoimage from 'dom-to-image';
 import AddValue from '../add-value';
 import { saveAs } from 'file-saver';
+import CheckIcon from '@material-ui/icons/Check';
+import { State } from '../../types/State';
 
 interface Props {
   currentTab: Tab;
   setCurrentTab: React.Dispatch<React.SetStateAction<Tab>>;
   showPreview: boolean;
-  togglePreview: () => void;
+  togglePreview: (set?: boolean) => void;
+  personalCount: number;
+  currentCount: number;
+  idealCount: number;
 }
 
 const Controls: React.FC<Props> = ({
@@ -20,11 +25,16 @@ const Controls: React.FC<Props> = ({
   setCurrentTab,
   showPreview,
   togglePreview,
+  personalCount,
+  currentCount,
+  idealCount,
 }) => {
   const [showAdd, setShowAdd] = useState(false);
   const dispatch = useDispatch();
   const handleResetValues = () => {
     dispatch(resetValues());
+    setCurrentTab(Tab.PERSONAL);
+    togglePreview(false);
   };
 
   const toggleShowAdd = () => {
@@ -34,17 +44,24 @@ const Controls: React.FC<Props> = ({
   const saveImage = () => {
     const modelDiv = document.getElementById('modelOutput');
     if (modelDiv) {
-      domtoimage.toBlob(modelDiv, { style: '' }).then((blob) => {
+      domtoimage.toBlob(modelDiv).then((blob) => {
         saveAs(blob, 'diagram.png');
       });
     }
   };
+
+  const handlePreviewButtonClick = () => {
+    togglePreview();
+  }
+
+  const isComplete = (numSelected: number) => numSelected > 9;
+
   return (
     <>
       <div className="flex justify-between">
         <div className="flex">
           <div className="mr-2">
-            <Button color="primary" variant="contained" onClick={togglePreview}>
+            <Button color="primary" variant="contained" onClick={handlePreviewButtonClick}>
               {showPreview ? 'Hide Preview' : 'Show Preview'}
             </Button>
           </div>
@@ -76,19 +93,26 @@ const Controls: React.FC<Props> = ({
               text="Personal"
               highlight={currentTab === Tab.PERSONAL}
               onClick={setCurrentTab}
-            />
+            >
+              {isComplete(personalCount) && <CheckIcon />}
+            </ControlButton>
+
             <ControlButton
               id={'current'}
               text={'Current'}
               highlight={currentTab === Tab.CURRENT}
               onClick={setCurrentTab}
-            />
+            >
+              {isComplete(currentCount) && <CheckIcon />}
+            </ControlButton>
             <ControlButton
               id={'ideal'}
               text={'Desired'}
               highlight={currentTab === Tab.IDEAL}
               onClick={setCurrentTab}
-            />
+            >
+              {isComplete(idealCount) && <CheckIcon />}
+            </ControlButton>
           </div>
         )}
       </div>
@@ -96,4 +120,10 @@ const Controls: React.FC<Props> = ({
   );
 };
 
-export default Controls;
+const mapStateToProps = (state: State) => ({
+  personalCount: state.personal.values.length,
+  currentCount: state.current.values.length,
+  idealCount: state.ideal.values.length,
+});
+
+export default connect(mapStateToProps)(Controls);
